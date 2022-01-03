164008f0839473b3aa12f1e6cf1d16319e47fb23
yudialfayat <ayudi3521@gmail.com>
2022-01-03 19:24:14 +0700

    Fix Bug

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { IProduct } from '../utils/interfaces';
 import { useProductsQuery } from '../generated/graphql';
 
 const Home: FC = () => {
-	const [{ data }] = useProductsQuery();
+	const [{ data, fetching, error }] = useProductsQuery();
 	const [products, setProducts] = useState<IProduct[] | null>(null);
 
 	useEffect(() => {
@@ -15,9 +15,9 @@ const Home: FC = () => {
 
 	return (
 		<div className="Home">
-			{!products ? (
-				<p>Loading...</p>
-			) : (
+			{fetching && <p>Loading...</p>}
+			{!fetching && !products && <p>Failed to fetch</p>}
+			{!fetching && products && (
 				<div className="flex gap-4 flex-wrap">
 					{data?.products.map((product) => {
 						return <ProductCard product={product} key={product.id} />;
