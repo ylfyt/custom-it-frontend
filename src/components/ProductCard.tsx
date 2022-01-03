8932f00000c64d3867ceb2c3b74263bfc7beb1c1
yudialfayat <ayudi3521@gmail.com>
2022-01-03 15:28:41 +0700

    Link for product-card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import { IProduct } from '../interfaces';
 
 interface ProductCardProps {
@@ -7,13 +8,15 @@ interface ProductCardProps {
 
 const ProductCard: FC<ProductCardProps> = ({ product }) => {
 	return (
-		<div className="ProductCard p-5 bg-gray-300 w-[200px]">
-			<img src={product.image} alt={product.name} />
-			<div>
-				<div>{product.name}</div>
-				<div>{product.price}</div>
+		<Link to={'/product/' + product.id}>
+			<div className="ProductCard p-5 bg-gray-300 w-[200px]">
+				<img src={product.image} alt={product.name} />
+				<div>
+					<div>{product.name}</div>
+					<div>{product.price}</div>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
 
